7cdc7b3d0bb0f01067d60abc1d6243cea4e894f0
최인규 <131370967+inq-o@users.noreply.github.com>
2025-03-07 13:58:43 +0900

    20250306

---
+++ src/com/study/march/week1/HelloJava.java
@@ -0,0 +1,8 @@
+package com.study.march.week1;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ src/com/study/march/week1/HelloJava2.java
@@ -0,0 +1,11 @@
+package com.study.march.week1;
+
+public class HelloJava2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello java1");
+        System.out.println("Hello java2");
+        System.out.println("Hello java3");
+    }
+
+}
